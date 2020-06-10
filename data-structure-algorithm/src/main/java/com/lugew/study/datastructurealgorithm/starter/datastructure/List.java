a945af6363561320248e9a721d391e51a0c3d26b
LuGew <lugew@outlook.com>
2020-06-10 16:13:36 +0800

    add list

--- data-structure-algorithm/src/main/java/com/lugew/study/datastructurealgorithm/starter/datastructure/List.java
+++ data-structure-algorithm/src/main/java/com/lugew/study/datastructurealgorithm/starter/datastructure/List.java
@@ -6,5 +6,22 @@ package com.lugew.study.datastructurealgorithm.starter.datastructure;
  * @author LuGew
  * @since 2020/6/10
  */
-public interface List extends LinearStructure {
+public interface List<T> extends LinearStructure {
+
+    void init();
+
+    int length();
+
+    int locate(T element);
+
+    T get(int index);
+
+    boolean insert(int index, T element);
+
+    T delete(int index);
+
+    String print();
+
+    boolean isEmpty();
+
 }
---
+++ data-structure-algorithm/src/main/java/com/lugew/study/list/AbstractList.java
@@ -0,0 +1,11 @@
+package com.lugew.study.list;
+
+import com.lugew.study.datastructurealgorithm.starter.datastructure.List;
+
+/**
+ * @author LuGew
+ * @since 2020/6/10
+ */
+public abstract class AbstractList implements List {
+
+}
