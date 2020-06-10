3034ccb3d191786bed178a90e278c91fbb92b2d4
LuGew <lugew@outlook.com>
2020-06-10 14:53:59 +0800

    init

---
+++ data-structure-algorithm/src/main/java/com/lugew/study/datastructurealgorithm/starter/DataStructure.java
@@ -0,0 +1,14 @@
+package com.lugew.study.datastructurealgorithm.starter;
+
+/**
+ * 数据结构
+ *
+ * @author LuGew
+ * @since 2020/6/10
+ */
+public class DataStructure {
+
+    private LogicalStructure logicalStructure;
+    private StorageStructure storageStructure;
+    private Operation operation;
+}
---
+++ data-structure-algorithm/src/main/java/com/lugew/study/datastructurealgorithm/starter/LogicalStructure.java
@@ -0,0 +1,9 @@
+package com.lugew.study.datastructurealgorithm.starter;
+
+/**
+ * @author LuGew
+ * @since 2020/6/10
+ */
+public class LogicalStructure {
+    private LinearStructure linearStructure;
+}
