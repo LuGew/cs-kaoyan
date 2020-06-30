2dbbca6997e561acb7d8fd11e5393af08ef233b2
lugew <lugew@outlook.com>
2020-06-30 13:29:05 +0800

    add computer network

---
+++ computer-network/src/main/java/com/lugew/cskaoyan/computernetwork/ComputerNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.lugew.cskaoyan.computernetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerNetworkApplication.class, args);
+    }
+
+}
---
+++ computer-network/src/test/java/com/lugew/cskaoyan/computernetwork/ComputerNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lugew.cskaoyan.computernetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
